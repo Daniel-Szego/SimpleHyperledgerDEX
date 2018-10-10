68416e3b1e671cc974ca4ee163119bb7d7982d66
Daniel-Szego <szego.daniel@gmail.com>
2018-10-10 12:58:07 +0200

    very much simplified matching algorithm

--- script/script.js
+++ script/script.js
@@ -29,7 +29,7 @@ async function ClearDataFunction(param) {
     console.log('clearing test data');
 
     // deleting assets
-    const dexReg = await getAssetRegistry(namespace + '.DEX'); 
+    const dexReg = await getParticipantRegistry(namespace + '.DEX'); 
     let dex = await dexReg.getAll();
     await dexReg.removeAll(dex);
 
@@ -76,7 +76,7 @@ async function CreateAndPlaceOrderFunction(param) {
 
     const newOrder = await factory.newResource(namespace, 'Order', numberOfOrders.toString());
     newOrder.orderStatus = "INITIATED";
-    newOrder.amount = param.amount;
+    newOrder.price = param.price;
     newOrder.assetType = param.assetType;
     newOrder.buyOrSell = param.buyOrSell;
     await orderReg.add(newOrder);       
@@ -89,28 +89,67 @@ async function CreateAndPlaceOrderFunction(param) {
 // Internal functions
 // Placing order internally
 async function PlaceOrderFunction (dex, order) {
-    const dexReg = await getAssetRegistry(namespace + '.DEX'); 		  
+    const dexReg = await getParticipantRegistry(namespace + '.DEX'); 		  
   	dex.orderBook.push(order);
     dexReg.update(dex);	
-    await MatchingFunction(dex);
+ //   await MatchingFunction(dex);
 }
 
-// matching oorder function of the decentralized exchange
-async function MatchingFunction (dex) {
+// matching order function of the decentralized exchange
+/**
+ *
+ * @param {org.simpledex.Match} param - model instance
+ * @transaction
+ */
+async function MatchingFunction (param) {
+  	let dex = param.dex;
 	console.log('start matching');
-	
+    const factory = getFactory(); 
+ 		
   	for(i=0; i < dex.orderBook.length; i++) {
-  		let actualOrder = orderBook[i];
-	  	console.log(actualOrder.orderId);
-      	
-      	
-        
-        
+      	if ((dex.orderBook[i].buyOrSell == true) && (dex.orderBook[i].orderStatus != "CLOSED")) {
+	  		let actualBuyOrder = dex.orderBook[i];
+		  	for(j=0; j < dex.orderBook.length; j++) {
+				if ((dex.orderBook[j].buyOrSell == false) && (dex.orderBook[j].orderStatus != "CLOSED")) {
+			  		let actualSellOrder = dex.orderBook[j];
+                	// matching asset type 
+                  	if (dex.orderBook[i].assetType == dex.orderBook[j].assetType) {       
+                        // matching amount
+	                  	if (dex.orderBook[i].price == dex.orderBook[j].price) {
+                          	console.log('matching');
+                         	//	creating matching event
+                          	let matchingEvent = factory.newEvent(namespace, 'MatchingEvent');
+		                    matchingEvent.dex = dex;
+		                    matchingEvent.price = dex.orderBook[j].price;
+  							matchingEvent.orders = new Array();
+  							matchingEvent.orders.push(dex.orderBook[i]);
+  							matchingEvent.orders.push(dex.orderBook[j]);
+    						await emit(matchingEvent);                            
+
+                          	const orderReg = await getAssetRegistry(namespace + '.Order'); 	
+
+                            dex.orderBook[i].orderStatus = "CLOSED";
+                            orderReg.update(dex.orderBook[i]);	
+
+                            const orderReg2 = await getAssetRegistry(namespace + '.Order'); 	
+                            dex.orderBook[j].orderStatus = "CLOSED";
+                            orderReg2.update(dex.orderBook[j]);	
+                          		                          		
+                          	// deleting element from the array
+                          	//delete dex.orderBook[i];
+                          	//delete dex.orderBook.remove[j];                           		
+                        }                      	
+                    }                  	
+                }      		
+            }
+        }
     }
   
 
   	console.log('end matching');
 }
+
+
 
 
 
